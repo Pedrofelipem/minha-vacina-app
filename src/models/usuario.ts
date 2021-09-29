c06a04f30c36b4b0551136e226d10b167af262e4
Pedro Felipe <felipexexeu02@gmail.com>
2021-09-29 18:42:50 -0300

    Models: atualizando e criando modelos

--- src/models/usuario.ts
+++ src/models/usuario.ts
@@ -1,12 +1,14 @@
 import { Municipio } from "./municipio";
+import { Permissao } from "./permissao";
 import { Vacina } from "./vacina";
 
 export interface Usuario {
-  nome: string,
-  municipio: Municipio,
-  dataNascimento: Date,
-  email: string,
-  senha: string,
-  vacinas?: Vacina[],
-  id?: number
+  nome: string;
+  municipio: Municipio;
+  dataNascimento: Date;
+  email: string;
+  senha: string;
+  vacinas?: Vacina[];
+  permissao?: Permissao[];
+  id?: number;
 }
